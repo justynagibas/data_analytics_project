0b9713118be6473e7a0fd35a43214f40423f9418
Jan Pomykacz <jasiek2miliony@gmail.com>
2024-03-15 10:05:21 +0100

    Description

--- async_fetch_data_tests.py
+++ async_fetch_data_tests.py
@@ -2,9 +2,10 @@ import aiohttp
 import asyncio
 import logging
 import time
-import requests
+import csv
+import os
 
-from requests.exceptions import JSONDecodeError, HTTPError
+from requests.exceptions import JSONDecodeError
 import pandas as pd
 
 # Read data to dataframe
@@ -30,14 +31,20 @@ outside_locations_df = df.loc[(df['Restaurant_latitude'] < india_low_lat) |
 
 df.drop(outside_locations_df.index, inplace=True)
 
-import concurrent.futures
+
 
 def setup_logging():
     logger = logging.getLogger(__name__)  # Get a logger for your module
     logger.setLevel(logging.DEBUG)  # Set the logging level
 
+    log_file_path = 'logs/route_problems.log'
+
+    # Check if the directory exists, if not create it
+    log_dir = os.path.dirname(log_file_path)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
     # Create a file handler for writing logs to a file
-    file_handler = logging.FileHandler('logs/route_problems.log')
+    file_handler = logging.FileHandler(log_file_path)
     file_handler.setLevel(logging.ERROR)  # Set the logging level for the file
 
     # Create a console handler for printing logs to the console
@@ -62,6 +69,7 @@ class OSMR_Client:
     def __init__(self) -> None:
         self.base_url = f'http://router.project-osrm.org/route/v1/driving/'
         self.suffix = '?overview=false&geometries=geojson&steps=true'
+        self.route_coordinates = dict()
 
     async def _fetch_data(self, session, record_idx, start_point, end_point):
         osrm_url = self.base_url + f'{start_point[1]},{start_point[0]};{end_point[1]},{end_point[0]}' + self.suffix
@@ -69,12 +77,13 @@ class OSMR_Client:
             async with session.get(osrm_url) as response:
                 response.raise_for_status()
                 data = await response.json()
-                return data
+                route_coordinates = [[(point[1], point[0]) for point in data['routes'][0]['legs'][0]['steps'][i]['geometry']['coordinates']] for i in range(len(data['routes'][0]['legs'][0]['steps']))]
+                distance = round(data['routes'][0]['distance']/1000, ndigits=4)
+                self.route_coordinates[record_idx] = (route_coordinates, distance)
         except (aiohttp.ClientError, JSONDecodeError) as error:
             logger.exception(f"Index: {record_idx}. Error: {error}. Route for points: start {start_point}, end {end_point}")
-            return None
 
-    async def get_routes(self, record_idxs, start_points, end_points):
+    async def get_routes(self, record_idxs, start_points, end_points, ):
         tasks = []
         async with aiohttp.ClientSession() as session:
             for start_point, end_point, record_idx in zip(start_points, end_points, record_idxs):
@@ -84,15 +93,27 @@ class OSMR_Client:
             return await asyncio.gather(*tasks, return_exceptions=True)
 
 client = OSMR_Client()
-
-df = df.head(2000)
+# messed indices from last run
+indices = [29661, 29681, 29656, 29651, 29639, 29682, 29662, 29687,
+           29690, 29653, 29691, 29670, 29669, 29674, 29672, 29676,
+           29671, 29678, 29675, 29673, 29680, 29677, 29679, 29692]
+df = df.loc[indices]
 
 async def main():
-    start_points = [(x1, y1) for x1, y1 in zip(df['Restaurant_longitude'], df['Restaurant_latitude'])]
-    end_points = [(x2, y2) for x2, y2 in zip(df['Delivery_location_longitude'], df['Delivery_location_latitude'])]
+    start_points = [(y1, x1) for x1, y1 in zip(df['Restaurant_longitude'], df['Restaurant_latitude'])]
+    end_points = [(y2, x2) for x2, y2 in zip(df['Delivery_location_longitude'], df['Delivery_location_latitude'])]
     record_idxs = df.index
-    results = await client.get_routes(record_idxs, start_points, end_points)
-    # Process results as needed
+    await client.get_routes(record_idxs, start_points, end_points)
+    result_path = "routes/routes3.csv"
+    result_dir = os.path.dirname(result_path)
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+    with open(result_path, 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=("Og-data-row-number", "Route-coordinates", "Distance [km]"))
+        writer.writeheader()
+        for key, val in client.route_coordinates.items():
+            writer.writerow({"Og-data-row-number": key, "Route-coordinates": val[0], "Distance [km]":val[1]})
+
 
 if __name__ == "__main__":
     start = time.time()
